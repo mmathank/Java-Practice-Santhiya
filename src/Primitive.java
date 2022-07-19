45f55099c6e3ffc8be8c367676d8ba27734a5ab1
mmathank <mathan.ambition@gmail.com>
2022-07-19 11:29:50 +0530

    "New Java File added - StringArraySort"

---
+++ src/Primitive.java
@@ -0,0 +1,25 @@
+public class Primitive {
+    public static void main(String [] args){
+        byte x = 23;
+        int y = 4454677;
+        short z =  -12367;
+        float a = 30.782f;
+        double o = 30.76858f+ 23+44567;
+        System.out.println(o);
+        char k = '\u0044';     //unicode
+        System.out.println(k);
+
+        int myFirstValue = 129;
+        float mySecondValue = (float) 30.9876767864566;
+        double myTotal = myFirstValue+mySecondValue;
+        System.out.println(myTotal);
+
+
+       int numberOfPounds= 98;
+       double convertedKg = numberOfPounds * 0.453592;
+        System.out.println("convertedKg is " + convertedKg);
+
+
+    }
+}
+
